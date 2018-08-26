50de7d248e5de8bb2de0e001ff4561d81aaa6ede
Aydar <dusmukhambetov@mail.ru>
2018-08-26 23:28:55 +0300

    Part 2 time - 41:00 Print frames with one color

---
+++ src/main/java/screensaver/Config.java
@@ -0,0 +1,25 @@
+package screensaver;
+
+import org.springframework.context.annotation.*;
+
+import java.awt.*;
+import java.util.Random;
+
+@Configuration
+@ComponentScan(basePackages = "screensaver")
+public class Config {
+    public static void main(String[] args) throws InterruptedException {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        while (true) {
+            context.getBean(ColorFrame.class).showOnRandomPlace();
+            Thread.sleep(50);
+        }
+    }
+
+    @Bean
+    @Scope("prototype")
+    public Color color() {
+        Random random = new Random();
+        return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+    }
+}
