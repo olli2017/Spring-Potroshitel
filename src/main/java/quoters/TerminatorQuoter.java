22e21d7342d0bd83a6f3df098221ae2c0ae46715
Aydar <dusmukhambetov@mail.ru>
2018-08-26 14:44:44 +0300

    time - 18:07 Create Annotation

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -1,6 +1,9 @@
 package quoters;
 
 public class TerminatorQuoter implements Quoter {
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
+
     private String message;
 
     public void setMessage(String message) {
@@ -8,6 +11,8 @@ public class TerminatorQuoter implements Quoter {
     }
 
     public void sayQuote() {
-        System.out.println("message = " + message);
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("message = " + message);
+        }
     }
 }
